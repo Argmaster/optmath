Fix initial distance calculation

source/optmath/HCA/HCA.py
@@ -66,15 +66,15 @@ class HCAStep:
         return new_distance_matrix
 
     def _indexes_to_reduce(self):
-        min_value = np.min(
-            self.distance_matrix[np.nonzero(self.distance_matrix)]
-        )
+        min_value = np.inf
+        index_pair = []
         for i, row in enumerate(self.distance_matrix):
             for j in range(i):
-                if row[j] == min_value:
-                    return [i, j], min_value
+                if row[j] < min_value:
+                    index_pair = (i, j)
+                    min_value = row[j]
 
-        return [], min_value
+        return index_pair, min_value
 
     def __str__(self) -> str:
         return f"Step({', '.join(str(c) for c in self.data)})"

source/optmath/HCA/distance/chebyshev.py
@@ -6,4 +6,4 @@ from .distance import DistanceBase
 
 class Chebyshev(DistanceBase):
     def __call__(self, first: RecordBase, second: RecordBase) -> float:
-        return np.max(first.numeric() - second.numeric())
+        return np.max(np.abs(first.numeric() - second.numeric()))

source/optmath/HCA/distance/manhattan.py
@@ -6,4 +6,4 @@ from .distance import DistanceBase
 
 class Manhattan(DistanceBase):
     def __call__(self, first: RecordBase, second: RecordBase) -> float:
-        return np.sum(first.numeric() - second.numeric())
+        return np.sum(np.abs(first.numeric() - second.numeric()))

source/optmath/HCA/distance_selector/ward.py
@@ -1,5 +1,23 @@
+from typing import List
+
+import numpy as np
+from numpy.typing import NDArray
+
+from ..cluster import Cluster
 from .selector import DistanceSelectorBase
 
 
 class Ward(DistanceSelectorBase):
-    pass
+    def new_distance_vector(
+        self,
+        to_reduce: List[int],
+        distance_matrix: NDArray[np.float64],
+        _: Cluster,
+        __: List[Cluster],
+    ):
+        old_size = len(distance_matrix)
+        return [
+            min(distance_matrix[to_reduce, i])
+            for i in range(old_size)
+            if i not in to_reduce
+        ]